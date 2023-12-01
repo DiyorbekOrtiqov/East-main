7595bed80a8075dee2d6527dbe5043e1d6304928
DiyorbekOrtiqov <ortiqovdiyorbek96@gmail.com>
2023-12-01 16:17:59 +0500

    addd

--- components/main/main.js
+++ components/main/main.js
@@ -17,6 +17,7 @@ const main = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10 overflow-y-auto">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto sms:pt-[100px] sm:pt-9">
           <MainText />
+           
         </div>
       </div>
       <div>
